45391b5f6245d3fdc30d5c2475e06fa505e6b0d8
Oleksandr Bondar <aleksandrr.bondar@gmail.com>
2023-08-02 01:23:02 +0300

    Add dbManager

---
+++ app/dbmanager/dbmanager.cpp
@@ -0,0 +1,183 @@
+#include "dbmanager.h"
+
+#include <QBuffer>
+#include <QByteArray>
+#include <QDebug>
+#include <QPixmap>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+DBManager::DBManager() : _db{ QSqlDatabase::addDatabase(QStringLiteral("QSQLITE")) }
+{
+    _db.setDatabaseName(QStringLiteral("dataBase.db"));
+    QSqlQuery query(_db);
+
+    if (!_db.open())
+    {
+        qWarning() << QStringLiteral("Database not open. Error: ") << _db.lastError().text();
+    }
+    else
+    {
+        query.exec(QStringLiteral(
+            "CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY, image, hash_sum INT"
+            "BLOB, similarity REAL)"));
+    }
+}
+
+DBManager::~DBManager()
+{
+    if (_db.isOpen())
+    {
+        _db.close();
+    }
+}
+
+int DBManager::newId() const
+{
+    QSqlQuery maxIdQuery("SELECT MAX(id) FROM images", _db);
+
+    if (maxIdQuery.exec() && maxIdQuery.next())
+    {
+        auto maxId = maxIdQuery.value(0).toInt();
+        return ++maxId;
+    }
+
+    return 0;
+}
+
+void DBManager::insertImageInToDataBase(QPixmap pixmap)
+{
+    QSqlQuery query(_db);
+    QByteArray byteArray;
+    QBuffer buffer(&byteArray);
+
+    buffer.open(QIODevice::WriteOnly);
+    pixmap.save(&buffer, "PNG");
+
+    const int nextId = newId();
+
+    query.prepare(
+        QStringLiteral("INSERT INTO images (id, image)"
+                       "VALUES (:id, :image)"));
+
+    query.bindValue(":id", nextId);
+    query.bindValue(":image", byteArray);
+
+    if (query.exec())
+    {
+        qDebug() << "Insertion in " << nextId << " image is successful";
+    }
+    else
+    {
+        qDebug("Insertion image is unsuccessful");
+        qDebug() << "Query error: " << query.lastError().text();
+    }
+}
+
+void DBManager::insertHashSumInToDataBase(const int hashSum, const int id)
+{
+    QSqlQuery query(_db);
+
+    query.prepare(QStringLiteral("UPDATE images SET hash_sum = :hash_sum WHERE id = :id"));
+    query.bindValue(":id", id);
+    query.bindValue(":hash_sum", hashSum);
+
+    if (query.exec())
+    {
+        qDebug("Update hash_sum is successful");
+    }
+    else
+    {
+        qDebug("Update hash_sum  is unsuccessful");
+        qDebug() << "Query error: " << query.lastError().text();
+    }
+}
+
+void DBManager::insertSimilarityBetweenImages(const float similarity, const int id) const
+{
+    QSqlQuery query(_db);
+
+    query.prepare(QStringLiteral("UPDATE images SET similarity = :similarity WHERE id = :id"));
+    query.bindValue(":id", id);
+    query.bindValue(":similarity", similarity);
+
+    if (query.exec())
+    {
+        qDebug("Update similarity is successful");
+    }
+    else
+    {
+        qDebug("Update similarity is unsuccessful");
+        qDebug() << "Query error: " << query.lastError().text();
+    }
+}
+
+DBManager::ImageData DBManager::imageData(const int id) const
+{
+    QSqlQuery query(_db);
+    ImageData tmpImageData;
+
+    qDebug() << "Take Last image from id = " << id;
+
+    query.prepare("SELECT id, image, hash_sum, similarity FROM images WHERE id = :id");
+    query.bindValue(":id", id);
+
+    if (query.exec() && query.next())
+    {
+        tmpImageData.currentId = query.value(0).toInt();
+
+        QByteArray byteArray = query.value(1).toByteArray();
+        QPixmap pixmap;
+        pixmap.loadFromData(byteArray);
+        tmpImageData.image = pixmap.toImage();
+
+        tmpImageData.hashSum = query.value(2).toFloat();
+        tmpImageData.similarity = query.value(3).toFloat();
+
+        qDebug("getLatestImageData()Reading latest image from database is successful");
+    }
+    else
+    {
+        qDebug("Reading latest image from database is unsuccessful");
+    }
+
+    return tmpImageData;
+}
+
+std::vector<DBManager::ImageData> DBManager::loadData() const
+{
+    std::vector<ImageData> data;
+
+    QSqlQuery query("SELECT id, image, hash_sum, similarity FROM images", _db);
+
+    while (query.next())
+    {
+        ImageData imageData;
+        const QByteArray byteArray = query.value(1).toByteArray();
+        QPixmap pixmap;
+
+        pixmap.loadFromData(byteArray);
+
+        imageData.currentId = query.value(0).toInt();
+        imageData.image = pixmap.toImage();
+        imageData.hashSum = query.value(2).toFloat();
+        imageData.similarity = query.value(3).toFloat();
+
+        data.push_back(imageData);
+    }
+
+    return data;
+}
+
+int DBManager::lastId() const
+{
+    QSqlQuery maxIdQuery("SELECT MAX(id) FROM images", _db);
+
+    if (maxIdQuery.exec() && maxIdQuery.next())
+    {
+        return maxIdQuery.value(0).toInt();
+    }
+
+    return 0;
+}
---
+++ app/dbmanager/dbmanager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QImage>
+#include <QObject>
+#include <QSqlDatabase>
+
+class DBManager : public QObject
+{
+    Q_OBJECT
+public:
+    struct ImageData
+    {
+        int currentId;
+        int previousId;
+        int hashSum;
+        QImage image;
+        float similarity;
+    };
+
+    DBManager();
+    ~DBManager();
+
+    int newId() const;
+    int lastId() const;
+    ImageData imageData(const int id) const;
+    std::vector<ImageData> loadData() const;
+
+    void insertImageInToDataBase(QPixmap pixmap);
+    void insertHashSumInToDataBase(const int hashSum, const int id);
+    void insertSimilarityBetweenImages(const float similarity, const int id) const;
+
+private:
+    QSqlDatabase _db;
+};
